542e9666135037c15470323bf872982d02fb01db
Diksha Bansal <92640652+Diksha-16@users.noreply.github.com>
2023-03-20 20:40:25 +0530

    Update Video.js

--- src/Video.js
+++ src/Video.js
@@ -22,7 +22,7 @@ import Modal from 'react-bootstrap/Modal'
 import 'bootstrap/dist/css/bootstrap.css'
 import "./Video.css"
 
-const server_url = process.env.NODE_ENV === 'production' ? 'https://my-microsoft-teams-clone.herokuapp.com/' : "http://localhost:4001"
+const server_url = process.env.NODE_ENV === 'production' ? 'https://video-call-bb3h.onrender.com/' : "http://localhost:4001"
 
 var connections = {}
 const peerConnectionConfig = {
